cb6d2042e4d58d548237644e04c3c008b69d3f64
Le Duc Nam <nam.le@watatek.com>
2026-08-18 00:00:41 +0700

    fix

--- vendor/frappe/hrms/frappe-ui/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  presets: [require('./src/tailwind/preset')],
-  content: ['./index.html', './App.vue', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
--- vendor/frappe/square_hrm_branding/square_hrm_branding/square_hrm_branding/__init__.py
+++
@@ -1,1 +0,0 @@
-# Module package required by Frappe for the "Square HRM Branding" module.
